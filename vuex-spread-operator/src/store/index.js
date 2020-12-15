0cc50f6e19958d1b1fe61862a5b7be34f454f91b
DaehunGwak <rhkrgns56@naver.com>
2020-12-16 01:10:02 +0900

    Add vuew helper test

--- vuex-spread-operator/src/store/index.js
+++ vuex-spread-operator/src/store/index.js
@@ -4,7 +4,14 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    dummy: "vuex dummy state"
+  },
+  getters: {
+    getDummy(state) {
+      return state.dummy;
+    }
+  },
   mutations: {},
   actions: {},
   modules: {}
